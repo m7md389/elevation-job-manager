a06b6bd4cda94408e1d2a35dcce21c83f59f0f54
Mohammad <m7md389@gmail.com>
2022-01-24 00:39:03 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Login from "./components/Login";
+import Login from "./components/PageNotFound";
+import Logout from "./components/PageNotFound";
+import Regester from "./components/PageNotFound";
 import PageNotFound from "./components/PageNotFound";
 import "./App.css";
 
@@ -11,9 +13,12 @@ export default function App() {
     <BrowserRouter>
       <NavBar />
 
-      <main>
-        <Routes></Routes>
-      </main>
+      <Routes>
+        <Route to="/login" element={<Login />} />
+        <Route to="/logout" element={<Logout />} />
+        <Route to="/register" element={<Regester />} />
+        <Route to="/page-not-found" element={<PageNotFound />} />
+      </Routes>
 
       <Footer />
     </BrowserRouter>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Link } from "react-router-dom";
 
 export default function Footer() {
   return <footer></footer>;

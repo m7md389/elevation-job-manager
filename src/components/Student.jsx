e50adac168ff06fe157b17f1b0a49dc344a34700
Mohammad <m7md389@gmail.com>
2022-01-25 10:29:11 +0200

    student css

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,98 +1,82 @@
-
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import '../styles/Student.css'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import "../styles/student.css";
 
 export default function Student() {
-    const companies = ['All companies', 'Yad2', 'Facebook', 'Twitter', 'Intel']
-    const statuses = ['All status', 'Accepted', 'waiting', 'Applied', 'no reply']
-    const [company, setCompany] = React.useState('');
+  const companies = ["All companies", "Yad2", "Facebook", "Twitter", "Intel"];
+  const statuses = ["All status", "Accepted", "waiting", "Applied", "no reply"];
+  const [company, setCompany] = React.useState("");
 
-    const handleChange = (event) => {
-        setCompany(event.target.value);
-    };
+  const handleChange = (event) => {
+    setCompany(event.target.value);
+  };
 
+  return (
+    <div className="student-page-container">
+      <div className="filters-detail">
+        <div className="cont">
+          <Box sx={{ minWidth: 120 }} className="box">
+            <FormControl fullWidth>
+              <InputLabel id="companies">Companies</InputLabel>
+              <Select
+                labelId="select-companies"
+                id="select-companies"
+                value={companies}
+                label="companies"
+                onChange={handleChange}
+              >
+                {companies.map((c, idx) => {
+                  return <MenuItem value={c}>{c}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+          </Box>
 
-    return (
-        <div className='student-page-container'>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="statuses">Statuses</InputLabel>
+              <Select
+                labelId="select-statuses"
+                id="select-statuses"
+                value={statuses}
+                label="statuses"
+                onChange={handleChange}
+              >
+                {statuses.map((s, idx) => {
+                  return <MenuItem value={s}>{s}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
 
+        <div className="right-box-details">
+          <p>name : </p>
+          <p>cohort : </p>
+          <p>email : </p>
+          <p>city : </p>
+          <p>phone : </p>
+        </div>
+      </div>
 
-            <div className='filters-detail'>
-                <div className='cont'>
-                    <Box sx={{ minWidth: 120 }} className='box'>
-                        <FormControl fullWidth>
-                            <InputLabel id="companies">Companies</InputLabel>
-                            <Select
-                                labelId="select-companies"
-                                id="select-companies"
-                                value={companies}
-                                label="companies"
-                                onChange={handleChange}>
+      <div style={{ width: "80%", margin: "10px auto" }}>
+        {" "}
+        <hr />
+      </div>
 
-                                {companies.map((c, idx) => {
-                                    return (
-                                        <MenuItem value={c}>{c}</MenuItem>
-                                    )
-                                })}
+      <div className="jobs-title">
+        <div>job name</div>
+        <div className="vLine">company</div>
+        <div className="vLine">date</div>
+        <div className="vLine">last interview</div>
+        <div className="vLine">status</div>
+      </div>
 
-
-                            </Select>
-                        </FormControl>
-                    </Box>
-
-                    <Box sx={{ minWidth: 120 }} >
-                        <FormControl fullWidth>
-                            <InputLabel id="statuses">Statuses</InputLabel>
-                            <Select
-                                labelId="select-statuses"
-                                id="select-statuses"
-                                value={statuses}
-                                label="statuses"
-                                onChange={handleChange}>
-
-                                {statuses.map((s, idx) => {
-                                    return (
-                                        <MenuItem value={s}>{s}</MenuItem>
-                                    )
-                                })}
-
-
-                            </Select>
-                        </FormControl>
-                    </Box>
-
-                </div>
-
-                <div className='right-box-details'>
-                    <p>name : </p>
-                    <p>cohort : </p>
-                    <p>email : </p>
-                    <p>city : </p>
-                    <p>phone : </p>
-                </div>
-            </div>
-
-            <div style={{ width: '80%', margin: '10px auto' }}> <hr /></div>
-
-            <div className='jobs-title'>
-                <div>job name</div>
-                <div className='vLine'>company</div>
-                <div className='vLine'>date</div>
-                <div className='vLine'>last interview</div>
-                <div className='vLine'>status</div>
-            </div>
-
-            <div className='rows'>
-
-            </div>
-
-
-
-        </div >
-    );
+      <div className="rows"></div>
+    </div>
+  );
 }
-

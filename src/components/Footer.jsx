022992db0d96ee0125d5bb1bf54bb459a7da40bd
Mohammad <m7md389@gmail.com>
2022-01-31 22:09:30 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import "../styles/footer.css";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="page-footer font-small cyan darken-3">
+      {/* <div className="container">
+        <div className="row">
+          <div className="col-md-12 py-5">
+            <div className="mb-5 flex-center">
+              <a className="fb-ic">
+                <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+              </a>
+              <a className="tw-ic">
+                <i className="fab fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <div className="footer-copyright text-center py-3">
+        © 2022 Copyright: <span className="team">xCoders</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

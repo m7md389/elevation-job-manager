d3c4478253eea645da2227cd8c02f6cc5ec3f84e
osamabayatra <osama_bayatra@hotmail.com>
2022-02-08 20:35:31 +0200

    changing localhost in login

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/api"
+  // baseURL: "http://localhost:3001/api"
+  baseURL: "/api"
 });
 
 instance.interceptors.response.use(null, (error) => {
